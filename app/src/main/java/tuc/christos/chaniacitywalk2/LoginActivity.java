75682c67e50d4218151fadbf22b04281b28312be
ChristosLP <cautionfail@gmail.com>
2017-04-07 18:57:20 +0300

    Fixed errors with Login Activity

--- app/src/main/java/tuc/christos/chaniacitywalk2/LoginActivity.java
+++ app/src/main/java/tuc/christos/chaniacitywalk2/LoginActivity.java
@@ -4,24 +4,25 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.support.v7.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import com.loopj.android.http.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -32,8 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
     // UI references.
+    private String mailToAutoComplete;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -41,9 +42,7 @@ public class LoginActivity extends AppCompatActivity {
 
     private TextView resultsView;
 
-    private final String[] DUMMY_CREDENTIALS = new String[]{
-            "cautionfail@gmail.com:hello"
-    };
+    private ArrayList<String> AUTO_COMPLETE_LIST = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,11 +63,7 @@ public class LoginActivity extends AppCompatActivity {
         mPasswordView = (EditText) findViewById(R.id.password);
 
         //add emails to autocomplete
-        String[] pieces = DUMMY_CREDENTIALS[0].split(":");
-        pieces[1] = "";
-        ArrayAdapter<String> adapter =new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, pieces);
-        mEmailView.setAdapter(adapter);
+        invokeWSGetAutoComplete();
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
@@ -79,7 +74,6 @@ public class LoginActivity extends AppCompatActivity {
             });
 
         if(autoSignIn){
-            mLoginFormView.setVisibility(View.GONE);
             //TODO:READ DATABASE, GET LOGIN INFO, ACCESS REMOTE SERVER AND VALIDATE
             //see alse Auth Task implementation
             invokeWSLogin("cautionfail@gmail.com", "hello");
@@ -88,10 +82,10 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void invokeWSGet(){
-        showProgress(true);
+    public void invokeWSGetAutoComplete(){
+
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://10.0.25.102:8080/Jersey/rest/contacts", null, new AsyncHttpResponseHandler() {
+        client.get("http://10.0.25.102:8080/Jersey/rest/players", null, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Header[] headers, byte[] bytes) {
                 String response = "";
@@ -100,50 +94,31 @@ public class LoginActivity extends AppCompatActivity {
                 }
                 try{
                     JSONObject json = new JSONObject(response);
-                    JSONArray contacts = json.getJSONArray("contact");
-
-                    String items = "";
-                    for(int j = 0; j < contacts.length(); j++) {
+                    JSONArray players = json.getJSONArray("player");
+                    for(int j = 0; j < players.length(); j++) {
                         //Decode contacts
-                        JSONObject contact = contacts.getJSONObject(j);
-                        String id = contact.getString("id");
-                        String name = contact.getString("name");
-                        int z = j+1;
-                        String temp= "\n\ncontact:"+z+"\n"+"name: "+ name+"\n"+"id: "+id;
-
-                        if( !contact.isNull("address") ) {
-                            //Decode Addresses
-                            JSONArray addresses = contact.getJSONArray("address");
-                            temp = temp +"\n"+"Addresses:";
-                            for (int y = 0; y < addresses.length(); y++) {
-                                JSONObject address = addresses.getJSONObject(y);
-                                String city = address.getString("city");
-                                String street = address.getString("street");
-                                int o = y + 1;
-                                String add = "\n\t" + o + ":" + city + "\t" + street;
-                                temp = temp + add;
-                            }
-                        }
-                        items = items +temp;
+                        JSONObject player = players.getJSONObject(j);
+                        String email = player.getString("email");
+                        AUTO_COMPLETE_LIST.add(email);
                     }
-                    //handleResponse(true, items);
+                    Log.i("JSONParse", "Parse Successful");
                 }catch(JSONException e){
-                   // handleResponse(true, e.getMessage());
+                    Log.i("JSONParse", e.getMessage());
                 }
+
+                ArrayAdapter<String> adapter =new ArrayAdapter<>(LoginActivity.this,
+                        android.R.layout.simple_dropdown_item_1line, AUTO_COMPLETE_LIST);
+                mEmailView.setAdapter(adapter);
             }
 
             @Override
             public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
-               // handleResponse(false,throwable.getMessage());
+                Log.i("GET Players", "GET FAILED");
             }
         });
     }
 
     private void attemptLogin(){
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -156,7 +131,11 @@ public class LoginActivity extends AppCompatActivity {
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        if (TextUtils.isEmpty(password) ) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }else if(!isPasswordValid(password)){
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
@@ -180,6 +159,7 @@ public class LoginActivity extends AppCompatActivity {
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+            mailToAutoComplete = email;
             invokeWSLogin(email,password);
             //mAuthTask = new UserLoginTask(email, password);
             //mAuthTask.execute((Void) null);
@@ -194,6 +174,7 @@ public class LoginActivity extends AppCompatActivity {
         client.get("http://10.0.25.102:8080/Jersey/rest/players/login&"+email+"&"+password, null , new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                showProgress(false);
                 String code = "";
                 for(byte b:bytes){
                     code = code + ((char)b);
@@ -203,6 +184,7 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                showProgress(false);
                 String code ="301";
                     handleResponse(code);
             }
@@ -210,30 +192,37 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     public void handleResponse(String response){
-        showProgress(false);
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
         boolean cancel = false;
         View focusView = null;
-
+        Log.i("Login Code:",response);
         switch(response){
             case "102":
                 mEmailView.setError(getString(R.string.error_incorrect_email));
                 resultsView.setText(R.string.action_sign_in_wrong);
+                AUTO_COMPLETE_LIST.remove(mailToAutoComplete);
                 focusView = mEmailView;
                 cancel = true;
                 break;
             case "201":
                 resultsView.setText(R.string.action_sign_in_successful);
+                AUTO_COMPLETE_LIST.add(mailToAutoComplete);
                 Intent intent = new Intent( getApplicationContext(), MapsActivity.class);
                 startActivity(intent);
                 break;
             case "202":
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 resultsView.setText(R.string.action_sign_in_wrong);
+                AUTO_COMPLETE_LIST.add(mailToAutoComplete);
                 focusView = mPasswordView;
                 cancel = true;
+                break;
+            case "301":
+                resultsView.setText(R.string.action_sign_in_server_error);
+                AUTO_COMPLETE_LIST.add(mailToAutoComplete);
+                cancel = false;
                 break;
         }
         if(cancel)
@@ -258,16 +247,15 @@ public class LoginActivity extends AppCompatActivity {
     private void showProgress(final boolean show) {
         int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
 
-        if(!(mLoginFormView.getVisibility()==View.GONE)) {
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+        mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
+                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
             });
-        }
+
         mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
         mProgressView.animate().setDuration(shortAnimTime).alpha(
                 show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
@@ -276,67 +264,7 @@ public class LoginActivity extends AppCompatActivity {
                 mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
             }
         });
+
     }
 
-    private class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        private String error = "";
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-            // TODO: attempt authentication against a network service.
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                error = "email";
-                if (pieces[0].equals(mEmail)) {
-                    error = "Password";
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-            // TODO: register the new account here.
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                Intent intent = new Intent( getApplicationContext(), MapsActivity.class);
-                startActivity(intent);
-            } else {
-                if(error.equals("Password")) {
-                    mPasswordView.setError(getString(R.string.error_incorrect_password));
-                    mPasswordView.requestFocus();
-                }else{
-                    mEmailView.setError(getString(R.string.error_incorrect_email));
-                    mEmailView.requestFocus();
-                }
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
